91707ce98c4224997b463dae7f68efcf1ad4f9a4
TylerArlington <131309046+TylerArlington@users.noreply.github.com>
2023-04-23 16:32:33 -0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -4,28 +4,61 @@
 //I put it all into its own header to keep it away from the actual game play stuff making organization easier
 //if yall dont like it in its own feel free to move it over to the main header
 
+//also im pretty much coding without focusing on making it actaully good so if you see anything thatll make for a better change please do fix
+
 
 class menu {
 public:
     //load main menu
     void MainMenu(RenderWindow& window)//make main menu
     {
+        Font font;
+        if (!font.loadFromFile("ARIAL.ttf"))
+        {
+            std::cout << "Failed to load image!" << std::endl;
+        }
+
+        //buttons--split by button and text overlay
+
         //play button
         RectangleShape play_button(sf::Vector2f(300, 100));
-        play_button.setPosition(450, 350);
+        play_button.setPosition(450, 150);
+        play_button.setFillColor(sf::Color::Transparent);
+        play_button.setOutlineThickness(2);
+        play_button.setOutlineColor(sf::Color::White);
 
+        Text playText("Start Game", font, 50);
+        playText.setPosition(450, 150);
+        playText.setFillColor(sf::Color::White);
+        
 
         //tutorial\rules\info button button
         RectangleShape info_button(sf::Vector2f(300, 100));
-        info_button.setPosition(450, 250);
+        info_button.setPosition(450, 350);
+        info_button.setFillColor(sf::Color::Transparent);
+        info_button.setOutlineThickness(2);
+        info_button.setOutlineColor(sf::Color::White);
+
+        Text infoText("Info", font, 50);
+        infoText.setPosition(450, 350);
+        infoText.setFillColor(sf::Color::White);
+
 
         //exit game button
         RectangleShape exit_button(sf::Vector2f(300, 100));
-        info_button.setPosition(450, 450);
+        exit_button.setPosition(450, 550);
+        exit_button.setFillColor(sf::Color::Transparent);
+        exit_button.setOutlineThickness(2);
+        exit_button.setOutlineColor(sf::Color::White);
+
+        Text exitText("Exit Game", font, 50);
+        exitText.setPosition(450, 550);
+        exitText.setFillColor(sf::Color::White);
+
 
         //makes menu background --  replace test.png with final background whenever ready
         Texture wallpaper;
-        if (!wallpaper.loadFromFile("test.png"))
+        if (!wallpaper.loadFromFile("menu1.1.png"))
         {
             std::cout << "Failed to load image!" << std::endl;
         }
@@ -45,19 +78,19 @@ public:
                     // Check if the mouse is over the start button
                     if (play_button.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
                     {
-                        //show info
+                        // play game
                     }
 
                     // Check if the mouse is over the info
-                    if (play_button.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                    if (info_button.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
                     {
                         //show info
                     }
 
                     // Check if the mouse is over the exit
-                    if (play_button.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                    if (exit_button.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
                     {
-                        //close game
+                        window.close();//close game
                     }
                 }
                 else if (event.type == Event::Closed)
@@ -71,6 +104,9 @@ public:
             window.draw(play_button);
             window.draw(info_button);
             window.draw(exit_button);
+            window.draw(playText);
+            window.draw(infoText);
+            window.draw(exitText);
             window.display();
         }
     }
