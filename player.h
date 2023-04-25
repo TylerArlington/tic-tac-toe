41dac9bd4afca75676fa395f01035ae695598197
TylerArlington <131309046+TylerArlington@users.noreply.github.com>
2023-04-25 13:15:17 -0700

    Update player.h

--- player.h
+++ player.h
@@ -1,13 +1,31 @@
-#pragma once
 
 #include "header.h"
 
-class player
-{
-protected:
 
+class player {
+protected:
+    // Add any protected member variables and functions here
 
 public:
+    virtual void makeMove() = 0;  // Define an abstract function to make a move
+};
 
+class humanPlayer : public player {
+protected:
+    // Add any protected member variables and functions here
 
+public:
+    void makeMove() override {
+        // Add code to prompt the human player to make a move and handle the input
+    }
+};
+
+class computerPlayer : public player {
+protected:
+    // Add any protected member variables and functions here
+
+public:
+    void makeMove() override {
+        // Add code to make a move for the computer player
+    }
 };
